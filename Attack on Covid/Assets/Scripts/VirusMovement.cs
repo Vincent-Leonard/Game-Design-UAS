8dc9196815c0429f489e8ea6e35f1c4ca31eec10
vleonard <62554949+Vincent-Leonard@users.noreply.github.com>
2021-01-11 21:14:36 +0700

    Virus Movement

---
+++ Attack on Covid/Assets/Scripts/VirusMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusMovement : MonoBehaviour
+{
+    float speed;
+    private Vector3 translation;
+    private Waypoints Wpoints;
+    private int waypointsIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+        speed = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointsIndex].position, speed);
+
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointsIndex].position) < 0.1f)
+        {
+            waypointsIndex++;
+        }
+
+    }
+}
