c0603e9e326f145b023a73590449ee7c717d71fe
Jonathan-Valentino <jonathanvalentinoprawira@yahoo.com>
2021-01-14 23:11:39 +0700

    Main Menu

---
+++ Attack on Covid/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetMouseButtonDown(0))
+        //{
+            //LoadNextLevel();
+        //}
+    }
+
+    public void OnBackToMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    public void OnPlayButton()
+    {
+        StartCoroutine(LoadLevel(1));
+    }
+
+    public void OnHowToPlayButton()
+    {
+        StartCoroutine(LoadLevel(2));
+    }
+
+
+    public void OnExitButton()
+    {
+        //game keluar
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        //StartCoroutine(LoadLevel(1));
+        //SceneManagement.LoadScene(SceneManagement.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator LoadLevel(int nextScene)
+    {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load Scene
+        SceneManager.LoadScene(nextScene);
+
+    }
+}
---
+++ Attack on Covid/Assets/Scripts/area.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class area : MonoBehaviour
+{
+    public GameObject prefabVirus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+       {
+          var newVirus = Instantiate(prefabVirus);
+          newVirus.transform.position = new Vector2(-8, 4);
+
+          RaycastHit  hit;
+          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          if (Physics.Raycast(ray, out hit)) {
+                 //if (hit.transform.name == "MyObjectName" )Debug.Log( "My object is clicked by mouse");
+          }
+       }
+        //bunyiin suara
+        //SoundController.instance.PlaySound(SFXType.LASER);
+    }
+
+    void OnMouseDown() 
+    {
+        //munculin slot
+        //var newVirus = Instantiate(prefabVirus);
+
+        //posisi slot
+        //newVirus.transform.position = new Vector2(5, 5);
+
+
+        //prefabVirus.active = true;
+        //teleporter.SetActive(true);
+    }
+}
